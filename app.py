54968a9976bb2912a1b875c37193231d0ef8d7f5
Toto3107 <mayankge147@gmail.com>
2025-07-04 07:31:22 +0000

    till helm

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+
+import os
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "hello de"
+    msg = os.getenv("welcome_msg","hello de")
+    return msg
 
 
 if __name__ == "__main__":
