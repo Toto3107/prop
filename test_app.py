9530d2f228f8b78ef26e9792651246efbcd11090
Toto3107 <mayankge147@gmail.com>
2025-07-06 15:19:47 +0000

    solved prob/

--- test_app.py
+++ test_app.py
@@ -5,5 +5,5 @@ def test_home():
     tester = app.test_client()
     response = tester.get("/")
     assert response.status_code == 200
-    assert b"hello de" in response.data
+    assert b"Mode: not set | Secret: no secret" in response.data
 
